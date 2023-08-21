e1da9d618dced6f44b59e43249dec876a3da7305
muntahaaa <bsse.1450@iit.du.ac.bd>
2023-08-21 14:26:05 -0400

    read contents from all files in directory and store the contents in vector

---
+++ FCB.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+struct FileControlBlock {
+    std::string filename;
+    std::vector<char> content;
+    std::size_t size;
+};
+
+bool loadFile(const std::string& filename, FileControlBlock& fcb) {
+    std::ifstream file(filename, std::ios::binary);
+    
+    if (!file.is_open()) {
+        std::cerr << "Error: Unable to open file '" << filename << "'" << std::endl;
+        return false;
+    }
+
+    file.seekg(0, std::ios::end);
+    fcb.size = static_cast<std::size_t>(file.tellg());
+    file.seekg(0, std::ios::beg);
+
+    fcb.content.resize(fcb.size);
+    file.read(fcb.content.data(), fcb.size);
+
+    fcb.filename = filename;
+
+    file.close();
+
+    return true;
+}
+
+void displayFileInformation(const FileControlBlock& fcb) {
+    std::cout << "File Name: " << fcb.filename << std::endl;
+    std::cout << "File Size: " << fcb.size << " bytes" << std::endl;
+}
+
+int main() {
+    FileControlBlock fcb;
+
+    // Replace 'example.txt' with the path to your file
+    if (loadFile("searched websites.txt", fcb)) {
+        displayFileInformation(fcb);
+    }
+
+    return 0;
+}
--- read_multiple_files.cpp
+++ read_multiple_files.cpp
@@ -7,16 +7,18 @@ using namespace filesystem;
 
 void read_file(string fileName){
    
-    ifstream inputFile(fileName); // Replace with file's name
+    ifstream inputFile(fileName); // Replace with file's name, ifstream for input function
 
     if (!inputFile.is_open()) {
        cerr << "Failed to open the file." << endl;
        
     }
-    char byte;
+    //to read data and represent it in Int form
+    char readData;
     bool flag=false;
-    while (inputFile.read(&byte, sizeof(byte))) {
-        //std::cout << static_cast<int>(byte) << " ";
+    while (inputFile.read(&readData, sizeof(readData))) {  //sizeof(readData)=1
+        //cout << static_cast<int>(readData) << " ";
+        //cout<<byte<<" ";
         flag=true;
     }
     if(flag) 
@@ -27,6 +29,8 @@ void read_file(string fileName){
 
 int main() {
     string directoryPath = "/home/muntaha/Desktop/SPL-01"; // Current directory
+//using const will prevent the entry variable to modify any content of the file
+//directory_iterator class will iterate through directory     
     for (const auto &entry : directory_iterator(directoryPath)) {
         if (is_regular_file(entry)) {
             read_file(entry.path().string());
