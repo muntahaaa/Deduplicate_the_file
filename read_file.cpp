d6a1a2adcf6f4dc8a8b4e1ac3c647916820e2e4f
muntahaaa <bsse.1450@iit.du.ac.bd>
2023-08-21 13:49:33 -0400

    read contents from all files in directory and store the contents in vector

--- read_file.cpp
+++ read_file.cpp
@@ -1,26 +1,91 @@
 #include <iostream>
-#include<string>
+#include <string>
 #include <fstream> // Required for file stream operations
+#include <string.h>
+#include <vector>
+#include <dirent.h>
+#include <sys/stat.h>
 using namespace std;
-bool flag=0;
-
-void read_file(string fileName){
-    ifstream inputFile(fileName); // Replace with file's name
-
-    if (!inputFile.is_open()) {
-       cerr << "Failed to open the file." << endl;
-       
+vector<string> fileContents;
+void listOfFilesInDirectory(const char *directorypath, vector<string> &paths)
+{
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(directorypath);
+    if (!dir)
+    {
+        cerr << "Error in opening directory " << directorypath << endl;
     }
-    char byte;
-    while (inputFile.read(&byte, sizeof(byte))) {
-        //std::cout << static_cast<int>(byte) << " ";
-        flag=1;
+    cout << "File and Directory names are: ";
+    while (entry = readdir(dir))
+    {
+
+        if (entry->d_type == DT_REG)
+        {
+            /*{
+                // regular file
+                cout << "File: " << entry->d_name << endl;
+            }
+            else if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0)
+            {
+                cout << "Directory: " << entry->d_name << endl;
+            }*/
+            string itemName = entry->d_name;
+            if (itemName != "." && itemName != "..")
+            {
+                string itemPath = string(directorypath) + "/" + itemName;
+                paths.push_back(itemPath);
+            }
+        }
     }
-    if(flag==1)  cout<<"byte code read successfully";
+    closedir(dir);
+}
+
+void read_file(string fileName, string &fileContent)
+{
+
+    ifstream inputFile(fileName); // Replace with file's name, ifstream for input function
+
+    if (!inputFile.is_open())
+    {
+        cerr << "Failed to open the file." << endl;
+    }
+
+    char readData;
+    bool flag = false;
+
+    // to read data and represent it in Int form
+    while (inputFile.read(&readData, sizeof(readData)))
+    {
+        // cout << static_cast<int>(readData) << " ";
+        //  cout<<byte<<" ";
+        flag = true;
+    }
+    if (flag)
+        cout << "\nFile " << fileName << " read successfully." << endl;
+    // storing contents in the vector
+    while (inputFile.get(readData))
+    {
+        fileContent += readData;
+    }
+    cout << "\n"
+         << fileName << " content loaded successfully";
 
     inputFile.close();
 }
 
-int main() {
-    read_file("SPL1 Project Proposal Final 1450.pdf");
+int main()
+{
+    const char *directoryPath = "/home/muntaha/Desktop/SPL-01"; // Current directory
+    vector<string> directoryItems;
+    listOfFilesInDirectory(directoryPath, directoryItems);
+
+    // creating a vector to store file contents
+
+    for (const string &itemPath : directoryItems)
+    {
+        string content;
+        read_file(itemPath, content);
+        fileContents.push_back(content);
+    }
 }
